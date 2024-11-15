4518ce7bd2fd030294f07e12a087e964449d9a39
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-11-15 08:09:21 +0100

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,8 @@
+﻿using Octokit.Webhooks.Events.PullRequest;
+using System.Diagnostics;
+
+PullRequestAction action = PullRequestAction.Assigned;
+string correct = (string)action;
+string wrong = action.ToString();
+
+Debugger.Break();
